e36dcbd2c5903c86b0cc77f474ac3d7bec1a7c2e
Keven Martins Ferraz Faria <145056753+KeaKzinho@users.noreply.github.com>
2023-11-21 16:56:46 -0300

    Delete Formulários directory

--- Formulários/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-      urlParams = new URLSearchParams(window.location.search);
-      const nome = urlParams.get('nome');
-      const senha = urlParams.get('senha');
-      document.getElementById('result').innerHTML = `Nome ${nome} Senha ${senha}`
-}
