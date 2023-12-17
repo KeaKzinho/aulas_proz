0131fd1342f92700011c6045695cb24bc47d87d6
Keven Martins Ferraz Faria <145056753+KeaKzinho@users.noreply.github.com>
2023-12-17 11:18:09 -0300

    Dicionários

---
+++ dicionario.py
@@ -0,0 +1,8 @@
+dados = {"nome": input("Informe seu nome: "),
+         "idade": int(input("Informe sua idade: ")),
+         "hobby": input("Informe seu hobby: ")}
+
+teste = input("Informe sua profissão: ")
+dados["Profissão"] = teste
+
+print(dados)
